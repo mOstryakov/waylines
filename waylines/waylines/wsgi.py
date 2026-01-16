32674039146b2265b0631e775bb23a8487337384
mOstryakov <mathews.ostryakov@yandex.ru>
2026-01-16 22:04:35 +0300

    update

--- waylines/waylines/wsgi.py
+++ waylines/waylines/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "waylines.waylines.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "waylines.settings")
 
 application = get_wsgi_application()
