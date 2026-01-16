ceaec0da6cb0488c3e3612f7aedeefb60b41ed19
mOstryakov <mathews.ostryakov@yandex.ru>
2026-01-16 22:13:29 +0300

    update hosts

--- waylines/waylines/settings.py
+++ waylines/waylines/settings.py
@@ -11,7 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [
+    "waylines.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 INSTALLED_APPS = [
